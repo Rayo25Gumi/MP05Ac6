8216b446085ab5755fe396a547e35345e19c15a0
Rayo25Gumi <marcguma@pm.me>
2025-03-30 22:14:07 +0200

    Final

--- Ac2/obj/Debug/net8.0/Ac2.AssemblyInfo.cs
+++ Ac2/obj/Debug/net8.0/Ac2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ac2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a333a41122e5898d5cd6a3c108dabd1ddfeb1038")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ac2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ac2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
